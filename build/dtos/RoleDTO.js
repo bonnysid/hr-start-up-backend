22361ccf7f8dcd6434b543f56cc28e9eaacb9aef
bortsovn <n.bortsov@softline.com>
2023-04-20 18:22:44 +0400

    Implement build ignore

--- build/dtos/RoleDTO.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RoleDTO {
-    constructor(model) {
-        this.id = model.id;
-        this.value = model.value;
-    }
-}
-exports.default = RoleDTO;
