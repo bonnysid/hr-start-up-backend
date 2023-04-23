a78082518a97f1f81dfc50e6a7d5777a39f6c202
bortsovn <n.bortsov@softline.com>
2023-04-23 20:14:05 +0400

    Implement post video upload

--- controllers/PostsController.ts
+++ controllers/PostsController.ts
@@ -2,6 +2,63 @@ import { Request, Response } from 'express';
 import PostModel, { PostStatus } from '../models/Post';
 import PostDTO from '../dtos/PostDTO';
 
+
+import multer from 'multer';
+import path from 'path';
+
+import { spawn  } from 'child_process';
+import { pipeline, Transform  } from 'stream';
+import fs from 'fs';
+import { v4 } from 'uuid';
+
+async function getVideoDuration(filePath: string): Promise<number> {
+  const ffprobe = spawn('ffprobe', [
+    '-v', 'error',
+    '-show_entries', 'format=duration',
+    '-of', 'default=noprint_wrappers=1:nokey=1',
+    filePath
+  ], { shell: true });
+
+  return new Promise((resolve, reject) => {
+    let stdout = '';
+    const parseDuration = new Transform({
+      readableObjectMode: true,
+      transform(chunk, encoding, callback) {
+        stdout += chunk.toString();
+        callback(null, chunk);
+      },
+      flush(callback) {
+        try {
+          resolve(parseFloat(stdout.trim()));
+        } catch (error) {
+          reject(error);
+        }
+        callback();
+      }
+    });
+
+    ffprobe.stdout?.on('error', (err: Error) => reject(err));
+    parseDuration.on('error', (err: Error) => reject(err));
+
+    pipeline(ffprobe.stdout, parseDuration, (err) => {
+      if (err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'videos'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${v4()}_${file.originalname}`);
+  },
+});
+
+export const upload = multer({ storage });
+
 class PostsController {
   async getPosts(req: Request, res: Response) {
     try {
@@ -36,6 +93,39 @@ class PostsController {
     } catch (e) {
       console.log(e);
       return res.status(500).json({ message: 'Server error' });
+    }
+  }
+
+  async createTestPost(req: Request, res: Response) {
+    const file = (req as any).file;
+
+    try {
+      const {
+        title,
+        description,
+        shortDescription,
+        tags,
+      } = req.body;
+
+      const user = (req as any).user;
+
+      const videoDuration = await getVideoDuration(file.path);
+
+      if (videoDuration > 10) {
+        fs.unlinkSync(file.path);
+        return res.status(400).json({ error: 'Длительность видео не должна привышать 10 секунд' });
+      }
+
+      const post = new PostModel({ title, description, shortDescription, tags, videoUrl: file.path, user: user.id });
+      await post.save();
+
+      const savedPost = post.populate('tags', 'user')
+
+      return res.json(new PostDTO(savedPost));
+    } catch (err) {
+      console.error(err);
+      fs.unlinkSync(file.path);
+      res.status(500).json({ error: 'Failed to upload video' });
     }
   }
 }
--- routers/PostsRouter.ts
+++ routers/PostsRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import PostsController  from '../controllers/PostsController';
+import PostsController, { upload }  from '../controllers/PostsController';
 import { authMiddleware } from '../middleware/AuthMiddleware';
+import bodyParser from 'body-parser';
 
 const router = Router();
 
 router.post('/create', authMiddleware, PostsController.createPost);
+router.post('/createTest', upload.single('video'), bodyParser.urlencoded({ extended: true }), authMiddleware, PostsController.createTestPost);
 router.get('/', PostsController.getPosts);
 
 export default router;
